9c6d35ad07b50d6792d66b59ec7235f8f0d335d7
Sergio Flores-Osorio <sergiofosorio@utexas.edu>
2024-02-21 20:15:39 -0600

    added directory structure and html files

---
+++ main.py
@@ -0,0 +1,12 @@
+from flask import Flask, render_template, request, redirect, url_for
+import requests
+
+app = Flask(__name__)
+
+@app.route('/')
+def render_index():
+    return render_template('index.html')
+
+if __name__ == '__main__':
+	app.debug = True
+	app.run(host = '127.0.0.1', port = 5000)
